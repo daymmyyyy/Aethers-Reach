d7200d93cd5fe7cf7388f287a7c65a621895e491
daymmyyyy <dayanafatinah@gmail.com>
2025-07-30 21:54:23 +1000

    added and implemented invincibility, made alpha of platforms/obstacles 0.5 when invincibility activated

--- Aethers_Reach/Assets/GameplayScripts/InvincibilityPowerUp.cs
+++ Aethers_Reach/Assets/GameplayScripts/InvincibilityPowerUp.cs
@@ -1,25 +1,76 @@
+using System.Collections;
 using UnityEngine;
 
 public class InvincibilityPowerUp : MonoBehaviour
 {
+    public float invincibilityDuration = 5f;
+    private float transparentAlpha = 0.5f;
+    private float originalAlpha = 1f;
+
     void OnTriggerEnter2D(Collider2D other)
     {
-            Destroy(other.gameObject);
-
-            DisableTaggedColliders("Obstacle");
-            DisableTaggedColliders("Ground");
+        if (other.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+            StartCoroutine(TemporaryDisableColliders());
+        }
     }
 
-    void DisableTaggedColliders(string tag)
+    IEnumerator TemporaryDisableColliders()
+    {
+        Collider2D[] obstacleColliders = DisableTaggedColliders("Obstacle");
+        Collider2D[] groundColliders = DisableTaggedColliders("Ground");
+
+        SetAlpha("Obstacle", transparentAlpha);
+        SetAlpha("Ground", transparentAlpha);
+
+        yield return new WaitForSeconds(invincibilityDuration);
+
+        EnableColliders(obstacleColliders);
+        EnableColliders(groundColliders);
+
+        SetAlpha("Obstacle", originalAlpha);
+        SetAlpha("Ground", originalAlpha);
+    }
+
+    Collider2D[] DisableTaggedColliders(string tag)
     {
         GameObject[] taggedObjects = GameObject.FindGameObjectsWithTag(tag);
+        Collider2D[] disabled = new Collider2D[taggedObjects.Length];
 
-        foreach (GameObject obj in taggedObjects)
+        for (int i = 0; i < taggedObjects.Length; i++)
         {
-            Collider2D col = obj.GetComponent<Collider2D>();
+            Collider2D col = taggedObjects[i].GetComponent<Collider2D>();
             if (col != null)
             {
                 col.enabled = false;
+                disabled[i] = col;
+            }
+        }
+
+        return disabled;
+    }
+
+    void EnableColliders(Collider2D[] colliders)
+    {
+        foreach (var col in colliders)
+        {
+            if (col != null)
+                col.enabled = true;
+        }
+    }
+
+    void SetAlpha(string tag, float alpha)
+    {
+        GameObject[] taggedObjects = GameObject.FindGameObjectsWithTag(tag);
+        foreach (GameObject obj in taggedObjects)
+        {
+            SpriteRenderer sr = obj.GetComponent<SpriteRenderer>();
+            if (sr != null)
+            {
+                Color c = sr.color;
+                c.a = alpha;
+                sr.color = c;
             }
         }
     }
