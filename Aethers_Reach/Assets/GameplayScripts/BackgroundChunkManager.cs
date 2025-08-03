0e3890cc5280189421519c0df15ddfcdddd5ebfc
daymmyyyy <dayanafatinah@gmail.com>
2025-08-03 21:52:27 +1000

    tried getting the bg chunks working

---
+++ Aethers_Reach/Assets/GameplayScripts/BackgroundChunkManager.cs
@@ -0,0 +1,67 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class BackgroundChunkManager : MonoBehaviour
+{
+    public static BackgroundChunkManager Instance;
+
+    [Header("Chunk Settings")]
+    public GameObject[] chunkPrefabs;
+    public Transform player;
+    public CinemachineVirtualCamera virtualCamera;
+    public int maxChunksOnScreen = 5;
+
+    private readonly List<GameObject> spawnedChunks = new();
+    private Transform lastChunkEnd;
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        SpawnNextChunk();
+    }
+
+    private void Update()
+    {
+        if (lastChunkEnd == null || virtualCamera == null) return;
+
+        Vector3 viewportPos = Camera.main.WorldToViewportPoint(lastChunkEnd.position);
+
+        if (viewportPos.x < 1.1f && viewportPos.x > 1f)
+        {
+            SpawnNextChunk();
+        }
+    }
+
+    public void SpawnNextChunk()
+    {
+        GameObject prefab = chunkPrefabs[Random.Range(0, chunkPrefabs.Length)];
+
+        GameObject newChunk = Instantiate(prefab);
+        Transform newChunkEnd = newChunk.transform.Find("ChunkEnd");
+
+        Vector3 spawnPosition = lastChunkEnd != null
+            ? new Vector3(lastChunkEnd.position.x, 14f, lastChunkEnd.position.z)
+            : new Vector3(0, 14f, 0);
+
+        newChunk.transform.position = spawnPosition;
+        lastChunkEnd = newChunkEnd;
+
+        spawnedChunks.Add(newChunk);
+        CleanupOldChunks();
+    }
+
+    private void CleanupOldChunks()
+    {
+        while (spawnedChunks.Count > maxChunksOnScreen)
+        {
+            Destroy(spawnedChunks[0]);
+            spawnedChunks.RemoveAt(0);
+        }
+    }
+}
